59da16498f81f4d2939cf1b7b431778b355a405a
Genki Kudo <96423408+genki-kudo@users.noreply.github.com>
2023-12-20 01:58:16 +0900

    Delete script directory

--- script/comp_stat_dataset/stat_correlation.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-def correlation_value():
-    mw_from_vol = json.loads(open(os.path.dirname(os.path.abspath(__file__))+'/vol_mw_fitting.json').read())
-    mw_from_dist = json.loads(open(os.path.dirname(os.path.abspath(__file__))+'/dist_passmw_fitting.json').read())
-
-    return mw_from_vol, mw_from_dist
