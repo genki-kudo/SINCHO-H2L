a614eca70098ca4075d40147631f6ac145bf076f
Genki Kudo <gxuanji@gmail.com>
2025-06-09 18:45:55 +0900

    [change] directory specify

--- yamlout.py
+++ yamlout.py
@@ -16,28 +16,28 @@ if __name__ == '__main__':
     ###load and preparation###
     with open(conditions,'r')as f:
         setting = yaml.safe_load(f)
-    in_dir = setting['P2C_SINCHO']['working_directory']
-    #####################################################
-    ##### "OUTPUT" will be changed its name #####
+    in_dir = os.path.join(setting['OUTPUT']['directory'], setting['SINCHO']['working_directory'])
     out_dir = setting['OUTPUT']['directory']
-    #####################################################
-    nums = setting['P2C_SINCHO']['num_of_parallel']
+    nums = setting['SINCHO']['num_of_parallel']
+    order_scale = int(len(str(int(nums)))+1)
 
-
+    """
     for i in range(nums+1):
-        n = str(i).zfill(3)
+        n = str(i).zfill(rank_order_scale)
         if not os.path.exists(out_dir+"trajectory_"+n+"/"):
             os.makedirs(out_dir+"trajectory_"+n+"/")
         for k in ["prot_","lig_"]:
             lower_file = "/trajectory_"+n+"/"+k+n+".pdb"
             bash("cp "+in_dir+lower_file+" "+out_dir+lower_file)
+    """
     
 
     
-    if list(setting['P2C_SINCHO']['output_method'].keys())[0]=="score_sort_evenly":
-        num = int(list(setting['P2C_SINCHO']['output_method'].values())[0])
+    if list(setting['SINCHO']['output_method'].keys())[0]=="score_sort_evenly":
+        num = int(list(setting['SINCHO']['output_method'].values())[0])
+        rank_order_scale = int(len(str(int(num)))+1)
         for i in range(nums+1):
-            n = str(i).zfill(3)
+            n = str(i).zfill(rank_order_scale)
             yml = {}
             summary = {}
             flag = 0
@@ -56,11 +56,11 @@ if __name__ == '__main__':
                     property["acceptor"]={"min":0, "max":3}
                     property["donor"]={"min":0, "max":3}
                     ## will be updated
-                    summary["rank_"+str(rank_index).zfill(3)]=property
+                    summary["rank_"+str(rank_index).zfill(rank_order_scale)]=property
                     rank_index +=1
                 
             yml['SINCHO_result']=summary
-            with open(out_dir+"trajectory_"+n+"/sincho_result.yaml","w")as y:
+            with open(in_dir+"trajectory_"+n+"/sincho_result.yaml","w")as y:
                 yaml.dump(yml, y, encoding='utf-8', allow_unicode=True)
 
         
